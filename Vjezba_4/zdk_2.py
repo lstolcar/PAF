978fb5e59ebe11255c5955144ce21da7677626a9
lstolcar <lstolcar@pmsft.hr>
2024-04-01 12:26:26 +0200

    jos samo tekst grafa

--- Vjezba_4/zdk_2.py
+++ Vjezba_4/zdk_2.py
@@ -1,19 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import particle as prt
-p1 = prt.Particle(10,60,0,0)
-D_a =(((p1.v_0)**2)/(-p1.ay))*np.sin(2*p1.kut)
+#p1 = prt.Particle(10,60,0,0)
+#D_a =(((p1.v_0)**2)/(-p1.ay))*np.sin(2*p1.kut)
 #p1.range()
 #D_n=p1.D_n
 #print(D_a)
 #print(D_n)
 lista_err=[]
 lista_dt=[]
-for t in np.linspace(0.01,0.1,300):
+for t in np.linspace(0.00001,0.1,300):
+    p1 = prt.Particle(10,60,0,0)
+    D_a =(((p1.v_0)**2)/(-p1.ay))*np.sin(2*p1.kut)
     p1.range(t)
-    Err = D_a - p1.D_n
+    Err =( (abs(D_a - p1.D_n))/D_a)*100
     lista_err.append(Err)
     lista_dt.append(t) 
+    p1.reset()
 
 plt.plot(lista_dt,lista_err)
+plt.ylabel('Absolute relative error [%]')
+plt.xlabel('dt [s]')
+#plt.figtext(0.3,0.5,'{}'.format('$ (Err)= \frac{|D_{analitical}-D_{numerical}}{D_{analitical}}\cdot 100% $'))
 plt.show()
