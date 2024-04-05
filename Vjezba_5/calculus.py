a4955cc86a94f8ca15fca77babccb7f20dab1db2
lstolcar <lstolcar@pmfst.hr>
2024-04-05 14:58:56 +0200

    vj5

---
+++ Vjezba_5/calculus.py
@@ -0,0 +1,32 @@
+import numpy as np
+import math as mtS
+
+def metoda_prva (func,T,eps=0.01,metoda='three-step'):
+    x=T
+    if metoda=='two-step':
+        derivacija_2=(func)
+
+
+
+        
+    
+
+def func():
+    return x**2+2*x+3 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+metoda_prva(func,2)
